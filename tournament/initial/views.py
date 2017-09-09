36b25299b78a628cbd46da83a091f00179dc20ab
Kerem Sevencan <keremsevencan@Kerems-MacBook-Pro-2.local>
2017-09-09 22:53:44 +0300

    each week each match teams takes random club

--- tournament/initial/views.py
+++ tournament/initial/views.py
@@ -3,10 +3,10 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 
-from .models import Season,Fixture
+from .models import Season,Fixture,Club
 from django.forms.models import model_to_dict
 
-from random import choice, shuffle, sample
+from random import choice
 
 from .fixture_generator import Generator
 
@@ -50,6 +50,7 @@ def makefixture(request, pk):
     # Load the season for the detail page
     season = Season.objects.get(pk=pk)
     teams = season.getTeams().all()
+    clubs = Club.objects.all()
     team_count = teams.count()
 
     generator = Generator(team_count)
@@ -57,12 +58,26 @@ def makefixture(request, pk):
 
     fixtures = []
 
+    startWeek = 1
+    clubList = clubs
+
     for match in matches:
+        if match['week'] != startWeek:
+            startWeek = match['week']
+            clubList = clubs
+
+        homeTeam = choice(clubList)
+        clubList = clubList.exclude(pk=homeTeam.id)
+        awayTeam = choice(clubList)
+        clubList = clubList.exclude(pk=awayTeam.id)
+
         fixture = {
             "season_id": season.id,
             "week": match['week'],
             "home_team_id": teams[match['home'] - 1].id,
+            "home_team_club": homeTeam.name,
             "away_team_id": teams[match['away'] - 1].id,
+            "away_team_club": awayTeam.name,
         }
         fixtures.append(fixture)
 
