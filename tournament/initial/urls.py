1549808d6c6146e130290cab327e0bda90b965ce
Kerem Sevencan <kerem@ritmix.org>
2017-09-15 14:49:19 +0300

    url updates

--- tournament/initial/urls.py
+++ tournament/initial/urls.py
@@ -3,9 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^seasons/$', views.list, name='list'),
-    url(r'^seasons/(?P<pk>[0-9]+)/$', views.detail, name="details"),
-    url(r'^seasons/(?P<pk>[0-9]+)/makefixture/$', views.makefixture, name="makefixture"),
-    url(r'^seasons/(?P<pk>[0-9]+)/scoreboard/$', views.scoreboard, name="scoreboard"),
+    url(r'^$', views.list, name='list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.detail, name="details"),
+    url(r'^(?P<pk>[0-9]+)/makefixture/$', views.makefixture, name="makefixture"),
+    url(r'^(?P<pk>[0-9]+)/scoreboard/$', views.scoreboard, name="scoreboard"),
 ]
--- tournament/tournament/urls.py
+++ tournament/tournament/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('initial.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^initial/', include('initial.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
